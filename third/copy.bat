485fdd3c73eedf6580adb78eea144759b1f5231d
xjp <qt00@qq.com>
2017-01-23 16:59:33 +0800

    NF:copy product to out

---
+++ third/copy.bat
@@ -0,0 +1,19 @@
+mkdir out
+cd out
+rm -rf bin
+rm -rf include
+rm -rf lib
+mkdir bin
+mkdir include
+mkdir lib
+
+cd ..
+copy .\openssl\libcrypto-1_1.dll .\out\bin
+copy .\openssl\libssl-1_1.dll .\out\bin
+
+xcopy .\curl\builds\libcurl-vc14-x86-release-dll-ssl-dll\*.* .\out\ /E
+
+copy .\libevent\build\bin\Debug\event.dll .\out\bin
+copy .\libevent\build\lib\Debug\event.lib .\out\lib
+
+pause
