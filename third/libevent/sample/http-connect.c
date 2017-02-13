53e8b95ce0fb48e8b8fb36d23c9648ceaae10e52
xjp <qt00@qq.com>
2017-02-13 16:58:02 +0800

    libevent http multi

--- third/libevent/sample/http-connect.c
+++ third/libevent/sample/http-connect.c
@@ -20,6 +20,22 @@ struct download_context {
 static void download_callback(struct evhttp_request *req, void *arg);
 static int download_renew_request(struct download_context *ctx);
 
+static void on_http_ok(struct download_context * ctx)
+{
+	struct evbuffer * data = 0;
+	data = ctx->buffer;
+
+	printf("got %d bytes\n", data ? evbuffer_get_length(data) : -1);
+
+	if (data)
+	{
+		const unsigned char * joined = evbuffer_pullup(data, -1);
+		printf("data itself:\n====================\n");
+		fwrite(joined, evbuffer_get_length(data), 1, stderr);
+		printf("\n====================\n");
+		evbuffer_free(data);
+	}
+}
 static void download_callback(struct evhttp_request *req, void *arg)
 {
 	struct download_context * ctx = (struct download_context*)arg;
@@ -33,7 +49,9 @@ static void download_callback(struct evhttp_request *req, void *arg)
 	switch (req->response_code)
 	{
 	case HTTP_OK:
-		event_base_loopexit(ctx->base, 0);
+		evbuffer_add_buffer(ctx->buffer, req->input_buffer);
+		on_http_ok(ctx);
+		//event_base_loopexit(ctx->base, 0);
 		break;
 	case HTTP_MOVEPERM:
 	case HTTP_MOVETEMP:
@@ -46,24 +64,25 @@ static void download_callback(struct evhttp_request *req, void *arg)
 		download_renew_request(ctx);
 		return;
 	default:/* failed */
-		event_base_loopexit(ctx->base, 0);
+		//event_base_loopexit(ctx->base, 0);
 		return;
 	}
 
-	evbuffer_add_buffer(ctx->buffer, req->input_buffer);
+	
 	ctx->ok = 1;
 }
 
-struct download_context * context_new(const char *url)
+struct download_context * context_new(const char *url, struct event_base * base)
 {
 	struct download_context * ctx = 0;
 	ctx = (struct download_context*)calloc(1, sizeof(struct download_context));
 	ctx->uri = evhttp_uri_parse(url);
 	if (!ctx->uri) return 0;
 
-	ctx->base = event_base_new();
+	ctx->base = base;
 	ctx->buffer = evbuffer_new();
 	ctx->dnsbase = evdns_base_new(ctx->base, 1);
+	ctx->ok = 0;
 
 	download_renew_request(ctx);
 	return ctx;
@@ -85,42 +104,44 @@ void context_free(struct download_context *ctx)
 
 static int download_renew_request(struct download_context *ctx)
 {
+	char path_query[1000] = {0};
 	int port = evhttp_uri_get_port(ctx->uri);
 	if (port == -1) port = 80;
 	if (ctx->conn) evhttp_connection_free(ctx->conn);
 
-	printf("host:%s, port:%d, path:%s\n", evhttp_uri_get_host(ctx->uri), port, evhttp_uri_get_path(ctx->uri));
+	printf("host:%s, port:%d, path:%s,query:%s\n", 
+		evhttp_uri_get_host(ctx->uri), 
+		port, 
+		evhttp_uri_get_path(ctx->uri),
+		evhttp_uri_get_query(ctx->uri));
+
 
 	ctx->conn = evhttp_connection_base_new(ctx->base, ctx->dnsbase, evhttp_uri_get_host(ctx->uri), port);
 	ctx->req = evhttp_request_new(download_callback, ctx);
-	evhttp_make_request(ctx->conn, ctx->req, EVHTTP_REQ_GET, evhttp_uri_get_path(ctx->uri));
+	
+	if (evhttp_uri_get_query(ctx->uri))
+	{
+		sprintf(path_query, "%s?%s", evhttp_uri_get_path(ctx->uri), evhttp_uri_get_query(ctx->uri));
+		evhttp_make_request(ctx->conn, ctx->req, EVHTTP_REQ_GET, path_query);
+	}
+	else
+	{
+		evhttp_make_request(ctx->conn, ctx->req, EVHTTP_REQ_GET, evhttp_uri_get_path(ctx->uri));
+	}
+	
 	evhttp_add_header(ctx->req->output_headers, "Host", evhttp_uri_get_host(ctx->uri));
 
 	return 0;
 }
 
-struct evbuffer *download_url(const char *url)
-{
-	struct download_context * ctx = context_new(url);
-	if (!ctx) return 0;
 
-	event_base_dispatch(ctx->base);
-
-	struct evbuffer * retval = 0;
-	if (ctx->ok)
-	{
-		retval = ctx->buffer;
-		ctx->buffer = 0;
-	}
-
-	context_free(ctx);
-	return retval;
-}
 
 int main(int argc, char **argv)
 {
-	struct evbuffer * data = 0;
-
+	
+	struct event_base * base = NULL;
+	struct download_context *ctx1 = NULL;
+	struct download_context *ctx2 = NULL;
 
 #ifdef WIN32
 	WORD wVersionRequested;
@@ -130,19 +151,12 @@ int main(int argc, char **argv)
 
 	(void)WSAStartup(wVersionRequested, &wsaData);
 #endif
+	base = event_base_new();
+	ctx1 = context_new("http://example.com/", base);
+	ctx2 = context_new("http://ip.taobao.com/service/getIpInfo.php?ip=218.24.136.211", base);
 
-	data = download_url("http://example.com/");
+	event_base_dispatch(base);
 
-	printf("got %d bytes\n", data ? evbuffer_get_length(data) : -1);
-
-	if (data)
-	{
-		const unsigned char * joined = evbuffer_pullup(data, -1);
-		printf("data itself:\n====================\n");
-		fwrite(joined, evbuffer_get_length(data), 1, stderr);
-		printf("\n====================\n");
-		evbuffer_free(data);
-	}
 
 	return 0;
 }
