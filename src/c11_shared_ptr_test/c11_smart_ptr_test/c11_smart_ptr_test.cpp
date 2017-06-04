2853b3b761ecfd3351dad8b056990662562d0610
xjp <qt00@qq.com>
2017-06-05 06:32:33 +0800

    test reverse iterator

--- src/c11_shared_ptr_test/c11_smart_ptr_test/c11_smart_ptr_test.cpp
+++ src/c11_shared_ptr_test/c11_smart_ptr_test/c11_smart_ptr_test.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <algorithm>
 using namespace std;
 
 
@@ -34,8 +35,34 @@ public:
 		cout << "del B";
 	}
 };
+
+void p_vec(vector<int> &vec)
+{
+	cout << "====" << endl;
+
+	for (auto i : vec)
+	{
+		cout << i << endl;
+	}
+}
+
+void test_reverse_it()
+{
+	
+	vector<int> vec{ 1,2,3,4,5 };
+	p_vec(vec);
+
+	vector<int>::reverse_iterator rit = find(vec.rbegin(), vec.rend(), 3);
+	//vec.insert(rit.base(), 999);
+	vec.erase((++rit).base());
+	p_vec(vec);
+};
+
 int main()
 {
+	test_reverse_it();
+	string ABBA("1234");
+	string BaaB(ABBA);
 	{
 		cout << std::is_convertible<B*, A*>::value;
 		std::shared_ptr<B> bbb(new B);
