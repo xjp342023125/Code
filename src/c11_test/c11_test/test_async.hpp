1f092a7de3522ffd96eb536cd6fb94873b2a0532
xjp <qt00@qq.com>
2017-07-23 17:16:21 +0800

    add google test

---
+++ src/c11_test/c11_test/test_async.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <condition_variable>
+#include <thread>
+#include <chrono>
+#include <queue>
+#include <future>
+using namespace std;
+
+
+struct X
+{
+	void foo(int a, const string)
+	{
+
+	}
+};
+
+static void test_async()
+{
+
+}
