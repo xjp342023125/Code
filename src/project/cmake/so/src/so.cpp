f83d4deec98addceeefbdc53b0a90b3a526131d4
xjp_work <qt00@qq.cpm>
2021-06-24 11:50:46 +0800

    so use lib

--- src/project/cmake/so/src/so.cpp
+++ src/project/cmake/so/src/so.cpp
@@ -1,21 +1,23 @@
 #include "so.h"
 #include <cstdio>
+#include  "lib.h"
 void so_in1(){
 	printf("so_in1\n");
+	
 }
 
 
 void so1(){
 	printf("so1\n");
+	printf("call lib1\n");
+	lib1();
 }
 
 void so2(){
 	printf("so2\n");
 }
 
-void solib_same_name(){
-	printf("solib_same_name in so\n");
-}
+
 
 void so_class1::so_class_f1()
 {
