0ed46e496d484f6349513c171a26522bd5c88eff
xjp_work <qt00@qq.cpm>
2021-07-05 15:55:36 +0800

    no use

--- src/ubsan/main.cpp
+++ src/ubsan/main.cpp
@@ -1,5 +1,15 @@
+#include <iostream>
+int xxxx;
+int xx(){
+	++xxxx;
+	xxxx = xxxx * 10;
+}
+
 int main(int argc, char **argv) {
+	xx();
 	  int k = 0x7fffffff;
 	    k += argc;
 	      return 0;
+
+		  printf("over\n");
 }
