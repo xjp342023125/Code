a6f6ae371fd157127b18b959c69ce0c2c76e6a22
xjp_work <qt00@qq.cpm>
2021-07-07 16:09:36 +0800

    add gcc8.0 no ret func test

---
+++ src/gcc8_test/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int xxxx;
+
+int xx();
+int xx22();
+void xx33();
+
+int main(){
+	xx();
+}
+
+int xx()
+{
+    xxxx += 999;
+}
+int xx22(){
+    xxxx += 999;
+    return  0;
+}
+void xx33(){
+    xxxx += 999;
+}
